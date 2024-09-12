a5719542bfad53c10774bf5e10f851358857008a
Сергей <master-fores@yandex.ru>
2024-09-12 17:13:29 +0300

    Добавлен stat-client

---
+++ stat-service/stat-client/src/main/java/ru/practicum/StatClient.java
@@ -0,0 +1,73 @@
+package ru.practicum;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+import static ru.practicum.Constants.FORMATTER;
+
+@Slf4j
+public class StatClient {
+    @Autowired
+    private final RestClient restClient;
+
+    public StatClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public void saveHit(String app, HttpServletRequest request) {
+        EndpointHitDto endpointHitDto = EndpointHitDto.builder()
+                .app(app)
+                .uri(request.getRequestURI())
+                .ip(request.getRemoteAddr())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        ResponseEntity<Void> response = restClient.post()
+                .uri("/hit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(endpointHitDto)
+                .retrieve()
+                .toBodilessEntity();
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            log.info("Сохранение информации о запросе");
+        } else {
+            log.error("Ошибка при сохранении информации");
+        }
+    }
+
+    public ResponseEntity<List<ViewStatsDto>> getStats(LocalDateTime start, LocalDateTime end,
+                                                       List<String> uris, boolean unique) {
+        log.info("Получение статистики для {}", uris);
+        try {
+            return restClient.get()
+                    .uri(uriBuilder ->
+                            uriBuilder.path("/stats")
+                                    .queryParam("start", start.format(FORMATTER))
+                                    .queryParam("end", end.format(FORMATTER))
+                                    .queryParam("uris", uris)
+                                    .queryParam("unique", unique)
+                                    .build())
+                    .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError,
+                            (request, response) ->
+                                    log.error("Ошибка при получении статистики для {} ", uris))
+                    .body(new ParameterizedTypeReference<>() {
+                    });
+        } catch (Exception e) {
+            log.error("Не удалось получить статистику для {}", uris, e);
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.SERVICE_UNAVAILABLE);
+        }
+    }
+}
