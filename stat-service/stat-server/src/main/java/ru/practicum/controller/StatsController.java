2908e79fb19d1b2a5c584a5de3deab00049184e2
Сергей <master-fores@yandex.ru>
2024-09-12 20:32:36 +0300

    test5556668889991255569990123

--- stat-service/stat-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stat-service/stat-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -1,7 +1,6 @@
 package ru.practicum.controller;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +22,7 @@ public class StatsController {
     }
 
     @GetMapping("stats")
-    public List<ViewStatsDto> getViewStats(@RequestParam String start,
+    public List<ViewStatsDto> findByParams(@RequestParam String start,
                                            @RequestParam String end,
                                            @RequestParam(required = false) List<String> uris,
                                            @RequestParam(required = false) boolean unique) {
