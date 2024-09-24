b6553444151745c34c98cb9bfae7cd49a9885339
Сергей <master-fores@yandex.ru>
2024-09-24 12:05:26 +0300

    test777888

---
+++ stat-service/stat-server/src/main/java/ru/practicum/service/StatsService.java
@@ -0,0 +1,12 @@
+package ru.practicum.service;
+
+import ru.practicum.EndpointHitDto;
+import ru.practicum.ViewStatsDto;
+
+import java.util.List;
+
+public interface StatsService {
+    void saveEndpointHit(EndpointHitDto endpointHitDto);
+
+    List<ViewStatsDto> getViewStats(String start, String end, List<String> uris, boolean unique);
+}
