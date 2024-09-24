19c66019c174b8d4bf84833bc25653030fb6e042
Сергей <master-fores@yandex.ru>
2024-09-24 20:29:49 +0300

    test123

--- stat-service/stat-client/src/main/java/ru/practicum/ErrorHandler.java
+++ stat-service/stat-client/src/main/java/ru/practicum/ErrorHandler.java
@@ -2,7 +2,6 @@ package ru.practicum;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,13 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        log.error(ex.getMessage());
-        return new ErrorResponse("Ошибка валидации", ex.getMessage());
-    }
-
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DateException.class)
     public ErrorResponse handleValidationException(DateException ex) {
