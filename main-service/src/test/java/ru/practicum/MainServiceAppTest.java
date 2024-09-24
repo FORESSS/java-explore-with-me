aee52016f2a73c852260380b3a98d287d9241cf2
Сергей <master-fores@yandex.ru>
2024-09-24 11:55:13 +0300

    test44456

--- main-service/src/test/java/ru/practicum/MainServiceAppTest.java
+++ main-service/src/test/java/ru/practicum/MainServiceAppTest.java
@@ -1,12 +1,8 @@
 package ru.practicum;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MainServiceAppTest {
-    @Test
-    void contextLoads() {
-    }
 
 }
