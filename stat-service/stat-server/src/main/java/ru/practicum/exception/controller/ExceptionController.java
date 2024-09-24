51e2527b81d87d08f38b34344c53bbb93df22c63
Сергей <master-fores@yandex.ru>
2024-09-24 20:03:11 +0300

    testqeqewqweq64545455555555

--- stat-service/stat-server/src/main/java/ru/practicum/exception/controller/ExceptionController.java
+++ stat-service/stat-server/src/main/java/ru/practicum/exception/controller/ExceptionController.java
@@ -1,14 +1,7 @@
 package ru.practicum.exception.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.practicum.exception.DataTimeException;
-import ru.practicum.exception.model.ApiError;
-
-import java.time.LocalDateTime;
 
 @RestControllerAdvice
 @Slf4j
