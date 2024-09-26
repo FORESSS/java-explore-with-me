6b36a51b2bb1a3857fd93b83d812176156452875
Сергей <master-fores@yandex.ru>
2024-09-26 17:32:02 +0300

    Внесены корректировки12345789000

--- stat-service/stat-client/src/main/java/ru/practicum/StatClient.java
+++ stat-service/stat-client/src/main/java/ru/practicum/StatClient.java
@@ -1,8 +1,8 @@
 package ru.practicum;
 
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
@@ -10,45 +10,58 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import ru.practicum.util.Validator;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
-import static ru.practicum.Constants.FORMATTER;
+import static ru.practicum.util.Constants.FORMATTER;
 
 @Component
 @Slf4j
 public class StatClient {
-
     private final RestClient restClient;
+    @Autowired
+    private final Validator validator;
 
-    public StatClient(@Value("${stat-server.url}") String serverUrl) {
+    public StatClient(@Value("${stat-server.url}") String serverUrl, Validator validator) {
         this.restClient = RestClient.create(serverUrl);
+        this.validator = validator;
         log.info("Server stat run URL: {}", serverUrl);
     }
 
-    @SneakyThrows
     public void saveHit(String app, HttpServletRequest request) {
-        log.info("Saving hit for {}", app);
-        EndpointHitDto endpointHitDto = toDto(app, request);
+        EndpointHitDto endpointHitDto = EndpointHitDto.builder()
+                .app(app)
+                .uri(request.getRequestURI())
+                .ip(request.getRemoteAddr())
+                .timestamp(LocalDateTime.now())
+                .build();
+
         ResponseEntity<Void> response = restClient.post()
                 .uri("/hit")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(endpointHitDto)
                 .retrieve()
                 .toBodilessEntity();
+
         if (response.getStatusCode().is2xxSuccessful()) {
-            log.info("Posted hit with code {}", response.getStatusCode());
+            log.info("Сохранение информации о запросе");
         } else {
-            log.error("Posted hit with error code {}", response.getStatusCode());
+            log.error("Ошибка при сохранении информации, код ошибки: {}", response.getStatusCode());
         }
-        Thread.sleep(500);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end,
                                        List<String> uris, boolean unique) {
-        log.info("Getting stats for {}", uris);
+        log.info("Получение статистики для {}", uris);
+        validator.checkDateTime(start, end);
         try {
             return restClient.get()
                     .uri(uriBuilder ->
@@ -61,21 +74,12 @@ public class StatClient {
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError,
                             (request, response) ->
-                                    log.error("Getting stats for {} with error code {}", uris, response.getStatusCode()))
+                                    log.error("Ошибка при получении статистики для {} ", uris))
                     .body(new ParameterizedTypeReference<>() {
                     });
         } catch (Exception e) {
-            log.error("Getting stats for {} failed", uris, e);
+            log.error("Не удалось получить статистику для {}", uris, e);
             return Collections.emptyList();
         }
-    }
-
-    private EndpointHitDto toDto(String app, HttpServletRequest request) {
-        return EndpointHitDto.builder()
-                .app(app)
-                .uri(request.getRequestURI())
-                .ip(request.getRemoteAddr())
-                .timestamp(LocalDateTime.now())
-                .build();
     }
 }
