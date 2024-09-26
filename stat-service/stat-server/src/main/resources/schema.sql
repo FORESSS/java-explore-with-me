65680000c1b78ba98df6e6b28af49dad12041c43
Сергей <master-fores@yandex.ru>
2024-09-26 14:39:33 +0300

    Внесены корректировки

--- stat-service/stat-server/src/main/resources/schema.sql
+++ stat-service/stat-server/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 create table if not exists statistics (
-  id integer generated always as identity primary key
+  id integer generated always as identity primary key,
   app varchar(128) not null,
   uri varchar(128) not null,
   ip varchar(16) not null,
-  timestamp timestamp not null,
+  timestamp timestamp not null
 );
