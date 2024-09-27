b76deb7a9c61773d4405e605596b219f413a8567
Сергей <master-fores@yandex.ru>
2024-09-27 22:21:53 +0300

    159000

--- main-service/src/main/java/ru/practicum/utils/Validator.java
+++ main-service/src/main/java/ru/practicum/utils/Validator.java
@@ -1,9 +1,16 @@
 package ru.practicum.utils;
 
 import lombok.RequiredArgsConstructor;
+import ru.practicum.exception.NotFoundException;
 import ru.practicum.user.repository.UserRepository;
 
 @RequiredArgsConstructor
 public class Validator {
     private final UserRepository userRepository;
+
+    public void checkUserId(long userId) {
+        if (!userRepository.existsById(userId)) {
+            throw new NotFoundException(String.format("Пользователь с id: %d не найден", userId));
+        }
+    }
 }
