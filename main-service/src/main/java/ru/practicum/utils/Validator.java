d3f9d63bed522e7f91cded667df6d3c83250e004
Сергей <master-fores@yandex.ru>
2024-09-27 18:10:27 +0300

    test compilation + category + validator

--- main-service/src/main/java/ru/practicum/utils/Validator.java
+++ main-service/src/main/java/ru/practicum/utils/Validator.java
@@ -1,7 +1,9 @@
 package ru.practicum.utils;
 
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 @RequiredArgsConstructor
 public class Validator {
 
