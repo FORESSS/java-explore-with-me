24cf0539430134ae8ddc0bfc3034d0d0422b7c41
Сергей <master-fores@yandex.ru>
2024-09-27 18:06:23 +0300

    test compilation + category + validator

--- main-service/src/main/java/ru/practicum/utils/Validator.java
+++ main-service/src/main/java/ru/practicum/utils/Validator.java
@@ -1,17 +1,8 @@
 package ru.practicum.utils;
 
 import lombok.RequiredArgsConstructor;
-import ru.practicum.category.repository.CategoryRepository;
-import ru.practicum.compilation.repository.CompilationRepository;
-import ru.practicum.event.repository.EventRepository;
-import ru.practicum.request.repository.RequestsRepository;
-import ru.practicum.user.repository.UserRepository;
 
 @RequiredArgsConstructor
 public class Validator {
-    private UserRepository userRepository;
-    private EventRepository eventRepository;
-    private RequestsRepository requestsRepository;
-    private CategoryRepository categoryRepository;
-    private CompilationRepository compilationRepository;
+
 }
