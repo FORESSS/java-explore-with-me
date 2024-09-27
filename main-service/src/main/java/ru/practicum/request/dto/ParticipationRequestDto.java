82d13b70edc15ec742c7d3e8048b12b9a4121422
Сергей <master-fores@yandex.ru>
2024-09-27 08:51:53 +0300

    test 0001

--- main-service/src/main/java/ru/practicum/request/dto/ParticipationRequestDto.java
+++ main-service/src/main/java/ru/practicum/request/dto/ParticipationRequestDto.java
@@ -1,4 +1,4 @@
-package ru.practicum.requests.dto;
+package ru.practicum.request.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AccessLevel;
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import ru.practicum.requests.model.Status;
+import ru.practicum.request.model.Status;
 import ru.practicum.utility.Constants;
 
 import java.time.LocalDateTime;
