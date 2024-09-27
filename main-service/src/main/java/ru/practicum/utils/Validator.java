08cace09bbd6ebd62165a091a11e512f96edd4fd
Сергей <master-fores@yandex.ru>
2024-09-27 19:50:04 +0300

    test compilation + category + validator0345 ---test

--- main-service/src/main/java/ru/practicum/utils/Validator.java
+++ main-service/src/main/java/ru/practicum/utils/Validator.java
@@ -1,8 +1,10 @@
 package ru.practicum.utils;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import ru.practicum.user.repository.UserRepository;
 
+@Component
 @RequiredArgsConstructor
 public class Validator {
     private final UserRepository userRepository;
