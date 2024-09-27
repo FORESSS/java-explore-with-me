5e90f587a6819128f7decc085c3ee738c8d7e216
Сергей <master-fores@yandex.ru>
2024-09-27 11:29:52 +0300

    test cat

--- main-service/src/main/java/ru/practicum/category/dto/RequestCategoryDto.java
+++ main-service/src/main/java/ru/practicum/category/dto/RequestCategoryDto.java
@@ -6,14 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.hibernate.validator.constraints.Length;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class NewCategoryDto {
+public class RequestCategoryDto {
     @NotBlank
-    @Length(min = 1, max = 50)
     String name;
 }
