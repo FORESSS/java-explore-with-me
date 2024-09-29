48f78e5800ab5c8d1690e36b043f8103099d9c6c
Сергей <master-fores@yandex.ru>
2024-09-29 18:10:13 +0300

    test test user

--- main-service/src/main/java/ru/practicum/user/dto/UserRequestDto.java
+++ main-service/src/main/java/ru/practicum/user/dto/UserRequestDto.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserRequestDto {
     @NotBlank
---
+++ main-service/src/test/java/ru/practicum/user/repository/UserRepositoryTest.java
@@ -0,0 +1,55 @@
+package ru.practicum.user.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import ru.practicum.user.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findUserByEmailTest() {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setName("Test User");
+        userRepository.save(user);
+        Optional<User> foundUser = userRepository.findUserByEmail("test@example.com");
+
+        assertNotNull(foundUser);
+        assertEquals(user.getEmail(), foundUser.get().getEmail());
+        assertEquals(user.getName(), foundUser.get().getName());
+    }
+
+    @Test
+    void findAllByIdInTest() {
+        User user1 = new User();
+        user1.setEmail("user1@example.com");
+        user1.setName("User 1");
+        userRepository.save(user1);
+
+        User user2 = new User();
+        user2.setEmail("user2@example.com");
+        user2.setName("User 2");
+        userRepository.save(user2);
+
+        List<Long> ids = List.of(user1.getId(), user2.getId());
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        Page<User> users = userRepository.findAllByIdIn(ids, pageRequest);
+
+        assertNotNull(users);
+        assertEquals(2, users.getContent().size());
+        assertEquals(user1.getEmail(), users.getContent().get(0).getEmail());
+        assertEquals(user2.getEmail(), users.getContent().get(1).getEmail());
+    }
+}
