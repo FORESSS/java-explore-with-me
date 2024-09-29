2765234adc821621a5d2b822560b3b5e08c55865
Сергей <master-fores@yandex.ru>
2024-09-29 16:48:53 +0300

    test test cat

---
+++ main-service/src/test/java/ru/practicum/category/controller/CategoryAdminControllerTest.java
@@ -0,0 +1,80 @@
+package ru.practicum.category.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.category.dto.CategoryDto;
+import ru.practicum.category.dto.RequestCategoryDto;
+import ru.practicum.category.service.CategoryService;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CategoryAdminController.class)
+public class CategoryAdminControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private CategoryService categoryService;
+    @Autowired
+    private ObjectMapper objectMapper;
+    private RequestCategoryDto requestCategoryDto;
+    private CategoryDto categoryDto;
+
+    @BeforeEach
+    public void setup() {
+        requestCategoryDto = new RequestCategoryDto();
+        requestCategoryDto.setName("Test Category");
+        categoryDto = new CategoryDto();
+        categoryDto.setId(1L);
+        categoryDto.setName("Test Category");
+    }
+
+    @Test
+    public void testAddCategory() throws Exception {
+        when(categoryService.addCategory(any(RequestCategoryDto.class)))
+                .thenReturn(categoryDto);
+        mockMvc.perform(post("/admin/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestCategoryDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Test Category")));
+
+        Mockito.verify(categoryService).addCategory(any(RequestCategoryDto.class));
+    }
+
+    @Test
+    public void testUpdateCategory() throws Exception {
+        when(categoryService.updateCategory(anyLong(), any(RequestCategoryDto.class)))
+                .thenReturn(categoryDto);
+
+        mockMvc.perform(patch("/admin/categories/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestCategoryDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Test Category")));
+
+        Mockito.verify(categoryService).updateCategory(anyLong(), any(RequestCategoryDto.class));
+    }
+
+    @Test
+    public void testDeleteCategory() throws Exception {
+        mockMvc.perform(delete("/admin/categories/1"))
+                .andExpect(status().isNoContent());
+
+        Mockito.verify(categoryService).deleteCategory(anyLong());
+    }
+}
---
+++ main-service/src/test/java/ru/practicum/category/repository/CategoryRepositoryTest.java
@@ -0,0 +1,54 @@
+package ru.practicum.category.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ru.practicum.category.model.Category;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class CategoryRepositoryTest {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void saveCategoryTest() {
+        Category category = new Category();
+        category.setName("Test Category");
+        Category savedCategory = categoryRepository.save(category);
+
+        assertNotNull(savedCategory.getId());
+        assertEquals(category.getName(), savedCategory.getName());
+    }
+
+    @Test
+    void findCategoryByNameTest() {
+        Category category = new Category();
+        category.setName("Test Category");
+        categoryRepository.save(category);
+        Optional<Category> foundCategory = categoryRepository.findCategoriesByNameContainingIgnoreCase("Test Category");
+
+        assertNotNull(foundCategory);
+        assertEquals(category.getName(), foundCategory.get().getName());
+    }
+
+    @Test
+    void findAllCategoriesTest() {
+        Category category1 = new Category();
+        category1.setName("Test Category 1");
+        categoryRepository.save(category1);
+
+        Category category2 = new Category();
+        category2.setName("Test Category 2");
+        categoryRepository.save(category2);
+        List<Category> categories = categoryRepository.findAll();
+
+        assertNotNull(categories);
+        assertEquals(2, categories.size());
+    }
+}
