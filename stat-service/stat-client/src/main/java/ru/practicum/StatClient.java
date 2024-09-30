9f1d4b0be3980c07bf92c5a27d118d84ee431ce2
Сергей <master-fores@yandex.ru>
2024-09-30 11:54:04 +0300

    Обновлен stat-client

--- stat-service/stat-client/src/main/java/ru/practicum/StatClient.java
+++ stat-service/stat-client/src/main/java/ru/practicum/StatClient.java
@@ -3,26 +3,32 @@ package ru.practicum;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import ru.practicum.util.StatValidator;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
-import static ru.practicum.Constants.FORMATTER;
+import static ru.practicum.util.StatConstants.FORMATTER;
 
+@Component
 @Slf4j
 public class StatClient {
-    @Autowired
     private final RestClient restClient;
+    @Autowired
+    private final StatValidator validator;
 
-    public StatClient(RestClient restClient) {
-        this.restClient = restClient;
+    public StatClient(@Value("${stat-server.url}") String serverUrl, StatValidator validator) {
+        this.restClient = RestClient.create(serverUrl);
+        this.validator = validator;
+        log.info("Server stat run URL: {}", serverUrl);
     }
 
     public void saveHit(String app, HttpServletRequest request) {
@@ -40,16 +46,18 @@ public class StatClient {
                 .retrieve()
                 .toBodilessEntity();
 
-        if (response.getStatusCode().is2xxSuccessful()) {
-            log.info("Сохранение информации о запросе");
-        } else {
-            log.error("Ошибка при сохранении информации, код ошибки: {}", response.getStatusCode());
+        validator.checkResponseStatus(response);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public ResponseEntity<List<ViewStatsDto>> getStats(LocalDateTime start, LocalDateTime end,
-                                                       List<String> uris, boolean unique) {
+    public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end,
+                                       List<String> uris, boolean unique) {
         log.info("Получение статистики для {}", uris);
+        validator.checkDateTime(start, end);
         try {
             return restClient.get()
                     .uri(uriBuilder ->
@@ -67,7 +75,7 @@ public class StatClient {
                     });
         } catch (Exception e) {
             log.error("Не удалось получить статистику для {}", uris, e);
-            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.SERVICE_UNAVAILABLE);
+            return Collections.emptyList();
         }
     }
 }
